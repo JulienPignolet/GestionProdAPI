e4f8c48b446a295869a7b28c66082189e3764325
yoanf <yoanfath@laposte.net>
2019-09-16 09:12:57 +0200

    Ajout DELETE / POST

--- src/main/java/univ/lorraine/GestionProdAPI/MovieRestController.java
+++ src/main/java/univ/lorraine/GestionProdAPI/MovieRestController.java
@@ -2,14 +2,15 @@ package univ.lorraine.GestionProdAPI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import univ.lorraine.GestionProdAPI.dao.FilmDAO;
 import univ.lorraine.GestionProdAPI.entity.Film;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
+@RequestMapping(("/movies"))
 public class MovieRestController {
     private FilmDAO filmDAO;
 
@@ -19,9 +20,43 @@ public class MovieRestController {
         this.filmDAO = filmDAO;
     }
 
-    @GetMapping("/movies")
+    @GetMapping
     public ResponseEntity<List<Film>> findAllMovies(){
         return  ResponseEntity.ok(filmDAO.findAll());
     }
+
+    /**
+     * Exemple de JSON
+     * {
+     "popularity": 54.766,
+     "vote_count": 2080,
+     "adult": false,
+     "original_language": "en",
+     "original_title": "Toy Stary GI",
+     "title": "Toy Story 4",
+     "vote_average": 7.7,
+     "overview": "Woody has always been confident about his place in the world and that his priority is taking care of his kid, whether that's Andy or Bonnie. But when Bonnie adds a reluctant new toy called \"Forky\" to her room, a road trip adventure alongside old and new friends will show Woody how big the world can be for a toy.",
+     "release_date": "2019-06-21"
+     }
+     * @param film
+     * @return
+     */
+    @PostMapping
+    public ResponseEntity create(@Valid @RequestBody Film film) {
+        return ResponseEntity.ok(filmDAO.save(film));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable Long id){
+        if (!filmDAO.findById(id).isPresent()) {
+            System.err.println("Id " + id + " is not existed");
+            ResponseEntity.badRequest().build();
+        }
+
+        filmDAO.deleteById(id);
+
+        return ResponseEntity.ok().build();
+    }
+
 
 }
