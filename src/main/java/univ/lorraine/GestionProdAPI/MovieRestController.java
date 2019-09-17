60b825ae864c98a34dd19c29afff4f02334e1d9e
Julien Pignolet <julien.pignolet@hotmail.fr>
2019-09-17 10:57:38 +0200

    Ajout get par id de film

--- src/main/java/univ/lorraine/GestionProdAPI/MovieRestController.java
+++ src/main/java/univ/lorraine/GestionProdAPI/MovieRestController.java
@@ -25,6 +25,9 @@ public class MovieRestController {
         return  ResponseEntity.ok(filmDAO.findAll());
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Film> findOneMovie(@PathVariable Long id) { return ResponseEntity.ok(filmDAO.findOneById(id)); }
+
     /**
      * Exemple de JSON
      * {
@@ -38,8 +41,6 @@ public class MovieRestController {
      "overview": "Woody has always been confident about his place in the world and that his priority is taking care of his kid, whether that's Andy or Bonnie. But when Bonnie adds a reluctant new toy called \"Forky\" to her room, a road trip adventure alongside old and new friends will show Woody how big the world can be for a toy.",
      "release_date": "2019-06-21"
      }
-     * @param film
-     * @return
      */
     @PostMapping
     public ResponseEntity create(@Valid @RequestBody Film film) {
--- src/main/java/univ/lorraine/GestionProdAPI/dao/FilmDAO.java
+++ src/main/java/univ/lorraine/GestionProdAPI/dao/FilmDAO.java
@@ -1,13 +1,17 @@
 package univ.lorraine.GestionProdAPI.dao;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import univ.lorraine.GestionProdAPI.entity.Film;
 
 import java.util.List;
 
+@Repository
 public interface FilmDAO extends CrudRepository<Film, Long> {
 
-    public List<Film> findByTitleLike(String name);
+    List<Film> findByTitleLike(String name);
 
-    public List<Film> findAll();
+    Film findOneById(Long id);
+
+    List<Film> findAll();
 }
