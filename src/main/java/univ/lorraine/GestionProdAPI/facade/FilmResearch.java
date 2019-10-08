e3caa92ba4ab8fb48e48c29342c9e49122e4f367
Julien Pignolet <julien.pignolet@hotmail.fr>
2019-10-08 16:46:22 +0200

    Début recherche (égalité et espaces)

---
+++ src/main/java/univ/lorraine/GestionProdAPI/facade/FilmResearch.java
@@ -0,0 +1,53 @@
+package univ.lorraine.GestionProdAPI.facade;
+
+import univ.lorraine.GestionProdAPI.entity.Film;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class FilmResearch {
+    public List<Film> research(List<Film> allFilms, String exp) {
+        List<Map.Entry<Film, Integer>> filmByScore = new ArrayList<>();
+        for (Film film : allFilms) {
+             int score = evalScore(film.getTitle(), exp);
+             if (score != -2) {
+                 if (film.getTitle() == "t eop")
+                     score = 100;
+                 filmByScore.add(new AbstractMap.SimpleImmutableEntry<>(film, score));
+             }
+        }
+        Collections.sort(filmByScore, Comparator.comparing(Map.Entry::getValue));
+        return filmByScore.stream()
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+    }
+
+    public int evalScore(String filmTitle, String exp) {
+        int max = -2;
+        while (filmTitle.length() >= exp.length()) {
+            int compare = compareTwoWords(filmTitle, exp, 0);
+            max = compare > max ? compare : max;
+            filmTitle = filmTitle.substring(1);
+        }
+        return max;
+    }
+
+    public int compareTwoWords(String filmTitle, String exp, int score) {
+        if (exp.length() == 0) {
+            return score;
+        } else {
+            int compare = compareTwoLetter(filmTitle.charAt(0), exp.charAt(0));
+            return  compare == -2 ? -2 : compareTwoWords(filmTitle.substring(1), exp.substring(compare == -1 ? 0 : 1), score + compare);
+        }
+    }
+
+    public int compareTwoLetter(char a, char b) {
+        if (a == ' ') {
+            return -1;
+        } else if (a == b) {
+            return 4;
+        } else {
+            return -2;
+        }
+    }
+}
---
+++ src/test/java/univ/lorraine/GestionProdAPI/FilmResearchTests.java
@@ -0,0 +1,30 @@
+package univ.lorraine.GestionProdAPI;
+
+import univ.lorraine.GestionProdAPI.entity.Film;
+import univ.lorraine.GestionProdAPI.facade.FilmResearch;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FilmResearchTests {
+    public static void main(String[] args) {
+        Film f1 = new Film();
+        f1.setTitle("Tête");
+        Film f2 = new Film();
+        f2.setTitle("tete");
+        Film f3 = new Film();
+        f3.setTitle("tacle");
+        Film f4 = new Film();
+        f4.setTitle("t eop");
+        List<Film> fList = new ArrayList<>();
+        fList.add(f4);
+        fList.add(f1);
+        fList.add(f2);
+        fList.add(f3);
+        FilmResearch fSearch = new FilmResearch();
+        fList = fSearch.research(fList, "te");
+        for (Film film : fList) {
+            System.out.println(film.getTitle());
+        }
+    }
+}
